95e50864109664d0d73345ac58918db2c6ad2173
diegobritto <diegodesouzabritto@gmail.com>
2022-09-02 00:41:11 -0300

    first commit

---
+++ comentarios/api/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from comentarios.api.serializers import ComentarioSerializer
+from comentarios.models import Comentario
+
+class ComentarioViewSet(ModelViewSet):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
